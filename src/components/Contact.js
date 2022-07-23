5e9f81f8b3b26b8bd13d3eacd22f89b6c300c1c6
Aditya Anil Mhaske <adityamhaske711@gmail.com>
2022-07-23 17:41:01 +0530

    Final Commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,7 @@
 import React   from 'react'
-// import { useLocation } from 'react-router-dom'
+
 export default function Contact() { 
-    // const location = useLocation();
-    // console.log(location);
+   
 
     return (
         <>
