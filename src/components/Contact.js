f7861f2a5d59dc4429d708ceae2d8b0abc87355e
Aditya Anil Mhaske <adityamhaske711@gmail.com>
2022-07-23 23:20:13 +0530

    Changes in User and Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,8 @@ export default function Contact() {
 
     return (
         <>
-    <h1> Hi main Contact Page par hun </h1>
+    <h1 className='display-3'> Welcome to IEEE Web ETP !!</h1>
+    <h1 className='display-4'> We are on contact page now</h1>
 </>
 
   )
--- src/components/User.js
+++ src/components/User.js
@@ -12,8 +12,9 @@ export default function Contact() {
 
   return (
     <>
-      <h1> Hi main User Page par hun </h1>
-      <p> My current location is {location.pathname}</p>
+      <h1 className='display-3'> Welcome to IEEE Web ETP !! </h1>
+      <h2 className='display-4'>We are on user page now </h2>
+      <p className='h3'> My current location is {location.pathname}</p>
       <button className="btn btn-primary mx-2" onClick={ () => goToContact()}>Go to Contact Page</button>
       <button className="btn btn-primary mx-2" onClick={ () => navigate(-1)}>Go Back</button>
     </>
