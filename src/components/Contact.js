7a811021cc4557adebaa1430c7c445c6f42978bb
Aditya Anil Mhaske <adityamhaske711@gmail.com>
2022-07-24 21:44:18 +0530

    Minor and Final Changes in code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,9 @@
-import React   from 'react'
+import React from "react";
 
-export default function Contact() { 
-   
-
-    return (
-        <>
-    <h1 className='display-3'> Welcome to IEEE Web ETP !!</h1>
-    <h1 className='display-4'> We are on contact page now</h1>
-</>
-
-  )
+export default function Contact() {
+  return (
+    <>
+      <h1 className="display-4"> We are on Contact page now</h1>
+    </>
+  );
 }
-
